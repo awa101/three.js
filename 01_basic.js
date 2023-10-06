41f8e0edbce5920d0d421816f5475aabb436583d
Yedam101 <109948144+Yedam101@users.noreply.github.com>
2023-10-06 13:54:05 +0900

    square

--- 01_basic.js
+++ 01_basic.js
@@ -22,6 +22,59 @@ class App{
 
         requestAnimationFrame(this.render.bind(this));
     }
+
+    _setupCamera() {
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            width / height,
+            0.1,
+            100
+        );
+        camera.position.z = 2;
+        this._camera = camera;
+    }
+
+    _setupLight() {
+        const color = 0xffffff;
+        const intensity = 2;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        this._scene.add(light);
+    }
+
+    _setupModel() { 
+        const geometry = new THREE.BoxGeometry(1, 1.1, 0.1); // 가로, 세로, 깊이
+        const material = new THREE.MeshPhongMaterial({color: "#FFA500"});
+
+        const cube = new THREE.Mesh(geometry, material);
+
+        this._scene.add(cube);
+        this._cube = cube;
+    }
+
+    resize() {
+        const width = this._divContainer.clientWidth;
+        const height = this._divContainer.clientHeight;
+
+        this._camera.aspect = width / height;
+        this._camera.updateProjectionMatrix();
+
+        this._renderer.setSize(width, height);
+    }
+
+    render(time) {
+        this._renderer.render(this._scene, this._camera);
+        this.update(time);
+        requestAnimationFrame(this.render.bind(this));
+    }
+
+    update(time) {
+        time *= 0.001; // second unit
+        this._cube.rotation.x = time;
+        this._cube.rotation.y = time;
+    }
 }
 
 window.onload = function() {
